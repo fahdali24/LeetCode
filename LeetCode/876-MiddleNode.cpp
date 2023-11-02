4d5f4cad2ff87e415f278bad6fea52ba7daaa0b7
fahdali24 <142120994+fahdali24@users.noreply.github.com>
2023-11-02 03:30:43 -0400

    Add files via upload

---
+++ LeetCode/876-MiddleNode.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+//LeetCode Problem 876 -Find Middle Node of the LinkedList
+class Node {
+    public:
+        int value;
+        Node* next;
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+class LinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        
+    public:
+        LinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+        }
+
+        ~LinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty";
+            } else {
+                while (temp != nullptr) {
+                    cout << temp->value;
+                    temp = temp->next;
+                    if (temp != nullptr) {
+                        cout << " -> ";
+                    }
+                }
+            }
+            cout << endl;
+        }
+
+        Node* getHead() {
+            return head;
+        }
+
+        Node* getTail() {
+            return tail; 
+        }
+        
+        void makeEmpty() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+            tail = nullptr;
+        }
+
+        void append(int value) {
+            Node* newNode = new Node(value);
+            if (head == nullptr) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+        
+        //This is the solution by the time the fast node reaches the end the slow node is in the middle
+        Node* findMiddleNode(){
+            Node* slow = head;
+            Node* fast = head;
+            while (fast != nullptr && fast->next != nullptr){
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            return slow;
+        }
+        
