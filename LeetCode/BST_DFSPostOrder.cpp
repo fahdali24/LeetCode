b9b32b335c9873e29e87bda9a6d2e3b69c2c8b69
fahdali24 <142120994+fahdali24@users.noreply.github.com>
2023-12-09 05:27:19 -0500

    Add files via upload

---
+++ LeetCode/BST_DFSPostOrder.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+//BST DFS PostOrder
+class Node { 
+    public: 
+        int value;
+        Node* left;
+        Node* right;
+
+        Node(int value) {
+            this->value = value;
+            left = nullptr;
+            right = nullptr;
+        }
+};
+
+
+class BinarySearchTree {
+    private:
+        Node* root;
+
+    public:
+        BinarySearchTree() { root = nullptr; }
+
+
+        // ---------------------------------------------------
+        //  This is a helper function used by the destructor
+        //  Deletes all nodes in BST
+        //  Similar to DFS PostOrder in Tree Traversal section
+        // ---------------------------------------------------
+        void destroy(Node* currentNode) {
+            if (currentNode == nullptr) return;
+            if (currentNode->left) destroy(currentNode->left);
+            if (currentNode->right) destroy(currentNode->right);
+            delete currentNode;
+        }
+
+        ~BinarySearchTree() { destroy(root); }
+
+        Node* getRoot() {
+            return root;
+        } 
+
+        bool insert(int value) {
+            Node* newNode = new Node(value);
+            if (root == nullptr) {
+                root = newNode;
+                return true;
+            }
+            Node* temp = root;
+            while(true) {
+                if (newNode->value == temp->value) return false;
+                if (newNode->value < temp->value) {
+                    if (temp->left == nullptr) {
+                        temp->left = newNode;
+                        return true;
+                    }
+                    temp = temp->left;
+                } else {
+                    if (temp->right == nullptr) {
+                        temp->right = newNode;
+                        return true;
+                    }
+                    temp = temp->right;
+                }
+            }
+        }
+         void DFSPostOrder(Node* currentNode) {
+            if (currentNode->left) {
+                DFSPostOrder(currentNode->left);
+            }
+            if (currentNode->right) {
+                DFSPostOrder(currentNode->right);
+            }
+            cout << currentNode->value << " ";
+        }
+
+        
+
+        
+        void DFSPostOrder() {
+            DFSPostOrder(root);
+        }
+
+};
